f7651d6c20d7003c55d0ce364eca9fbaffa154e5
Bernardo Paz de Freitas <125703112+b3rnard0p@users.noreply.github.com>
2024-12-04 21:50:39 -0300

    Delete PrimeiraAPI directory

--- PrimeiraAPI/src/main/java/com/example/primeiraapi/repository/AlunoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.primeiraapi.repository;
-
-import com.example.primeiraapi.model.Aluno;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AlunoRepository extends CrudRepository<Aluno, Integer> {
-}
